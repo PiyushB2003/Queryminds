3e362ef176b2dc3e3b2d1bd9435ab0dc599c6a66
Piyush <piyushborkar95@gmail.com>
2024-08-30 14:09:39 +0530

    Creating verify route for payment verification

--- backend/Routes/PaymentRouter.js
+++ backend/Routes/PaymentRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Razorpay from "razorpay";
 import crypto from "crypto";
+import { Payment } from "../models/Payment.js";
 
 const PaymentRouter = express.Router();
 
@@ -22,16 +23,41 @@ PaymentRouter.post("/order", (req, res) => {
         razorpayInstance.orders.create(options, (error, order) => {
             if (error) {
                 console.log("Order payment error: ", error);
-                return res.status(500).json({ message: "Something went wrong while payment", success: false, error })
+                return res.status(500).json({ message: "Something went wrong while payment", success: false, error });
             }
             console.log(order);
-            return res.status(200).json({data: order, success: true});
-            
+            return res.status(200).json({ data: order, success: true });
+
         })
     } catch (error) {
-        console.log("Error occured in order: ", error);
+        console.log("Order route catch error: ", error);
         return res.status(500).json({ message: "Internal server error", success: false, error })
     }
 })
 
+PaymentRouter.post("/verify", async (req, res) => {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+    try {
+        const sign = razorpay_order_id + "|" + razorpay_payment_id;
+
+        const expectedSign = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+
+        console.log(razorpay_signature === expectedSign);
+
+        const isAuthentic = razorpay_signature === expectedSign;
+        if (isAuthentic) {
+            const payment = new Payment({
+                razorpay_order_id,
+                razorpay_payment_id,
+                razorpay_signature
+            })
+            await payment.save();
+            return res.status(200).json({ message: "Payment successful", success: true })
+        }
+
+    } catch (error) {
+        console.log("Verify route catch error: ", error);
+        return res.status(500).json({ message: "Internal server error", success: false, error });
+    }
+})
 export default PaymentRouter;
